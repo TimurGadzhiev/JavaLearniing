be1f243e4a4c1a22addd64cb53c0cef2bb6f900d
TimurGadzhiev <timur.gadzhiev07@gmail.com>
2023-07-17 21:24:27 +0200

    addressed code review comments x2

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int amountColumns = scanner.nextInt();
         int amountLines = scanner.nextInt();
-        for (int i = 1; i <= amountColumns; i++) {
-            for (int k = 1; k <= amountLines; k++) {
-                if (i == amountLines) {
-                    System.out.println(k * i);
-                    System.out.println();
-                    return;
+        int amountColumns = scanner.nextInt();
+        for (int i = 1; i <= amountLines; i++) {
+            for (int j = 1; j <= amountColumns; j++) {
+                if (j == 1) {
+                    System.out.print(i * j);
+                } else if (j > 1) {
+                    System.out.print(" " + i * j);
                 }
-                System.out.print(k * i + " ");
+
             }
             System.out.println();
         }
