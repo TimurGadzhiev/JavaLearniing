09f03f90ddc4c79af798c17cbf8e20137b019564
TimurGadzhiev <timur.gadzhiev07@gmail.com>
2023-07-21 20:58:17 +0200

    Max digit from number x2

--- src/MaxDigitFromNumber.java
+++ src/MaxDigitFromNumber.java
@@ -3,18 +3,11 @@ import java.util.Scanner;
 public class MaxDigitFromNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+        int number = Math.abs(scanner.nextInt());
         int maxDigit = number % 10;
-        String temp = Integer.toString(number);
-        int length = temp.length();
-        int a = 10;
-        int b = 1;
-        for (int i = 0; i < length; i++) {
-            if ((number % a) / b > maxDigit) {
-                maxDigit = (number % a) / b;
-            }
-            a *= 10;
-            b *= 10;
+        while (number != 0) {
+            number /= 10;
+            maxDigit = Math.max(number % 10, maxDigit);
         }
         System.out.println(maxDigit);
     }
