7f0ed094bbd581f5d57a47d725cfa59ddc5e3254
TimurGadzhiev <timur.gadzhiev07@gmail.com>
2023-07-25 02:17:20 +0200

    Addressed to comments

--- src/Sequence.java
+++ src/Sequence.java
@@ -7,8 +7,13 @@ public class Sequence {
         int count = 0;
         for (int i = 1; count < n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(" " + i);
-                count++;
+                if (i == 1) {
+                    System.out.print(i);
+                    count++;
+                } else {
+                    System.out.print(" " + i);
+                    count++;
+                }
                 if (count == n) {
                     break;
                 }
